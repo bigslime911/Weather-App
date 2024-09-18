a8042d444238c18917ad2bfc1845558355aefad9
osei <codewithkevin709@gmail.com>
2024-09-18 10:57:19 +0000

    first commit

---
+++ src/Screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import { View, Text} from 'react-native'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { Image } from 'react-native'
+
+const HomeScreen = () => {
+  return (
+    <View className='flex-1 relative'>
+      <StatusBar style='light'/>  
+            
+    </View>
+    
+  )
+}
+
+export default HomeScreen
